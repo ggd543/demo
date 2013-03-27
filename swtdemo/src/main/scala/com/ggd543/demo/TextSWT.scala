c84f336bbaf1db15909ef50b80264645253e886d
liuyongjian <ggd543@gmail.com>
2013-03-27 17:44:17 +0800

    x

---
+++ swtdemo/src/main/scala/com/ggd543/demo/TextSWT.scala
@@ -0,0 +1,10 @@
+/*
+ * @(#) TextSWT.java
+ * @Author:liuyongjian(liuyongjian@travelsky.com) 2013-03-27
+ * @Copyright (c) 2002-2013 Travelsky Limited. All rights reserved.
+ */
+package com.ggd543.demo
+
+object TextSWT {
+
+}
