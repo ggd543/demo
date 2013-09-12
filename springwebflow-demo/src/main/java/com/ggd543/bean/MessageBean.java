0ce6f808be777c32127f9bf629986ce6bccd7bc8
liuyongjian <ggd543@gmail.com>
2013-09-12 19:52:20 +0800

    .

---
+++ springwebflow-demo/src/main/java/com/ggd543/bean/MessageBean.java
@@ -0,0 +1,14 @@
+package com.ggd543.bean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-9-12
+ * Time: 下午7:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessageBean {
+    public void say(String message) {
+        System.out.println("message : " + message);
+    }
+}
