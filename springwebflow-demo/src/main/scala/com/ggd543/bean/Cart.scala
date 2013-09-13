c9c0313371d33411b388b6b475faa0c1ab540eb7
liuyongjian <ggd543@gmail.com>
2013-09-13 10:53:09 +0800

    del j2eedemo

---
+++ springwebflow-demo/src/main/scala/com/ggd543/bean/Cart.scala
@@ -0,0 +1,12 @@
+package com.ggd543.bean
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-9-12
+ * Time: 下午7:56
+ * To change this template use File | Settings | File Templates.
+ */
+class Cart {
+
+}
---
+++ springwebflow-demo/src/main/scala/com/ggd543/bean/Fuxk.scala
@@ -0,0 +1,12 @@
+package com.ggd543.bean
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-9-12
+ * Time: 下午8:01
+ * To change this template use File | Settings | File Templates.
+ */
+class Fuxk {
+
+}
